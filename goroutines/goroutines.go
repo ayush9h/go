87c7cabd8b840d96ca55ce227e69a67691bae796
Ayush <ayush.kumar.21cse@bmu.edu.in>
2026-06-26 20:06:32 +0530

    init waitgroups

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func checkNums(i int) {
+func checkNums(i int, wg *sync.WaitGroup) {
+	defer wg.Done() // runs after completion of the function with defer keyword
 	fmt.Println(i)
 }
 
 func main() {
+
+	var wg sync.WaitGroup
 	for i := 0; i <= 10; i++ {
-		go checkNums(i)
+		wg.Add(1)
+		go checkNums(i, &wg)
 	}
 
-	time.Sleep(time.Second * 2)
+	wg.Wait()
+	// Bad Habit
+	// time.Sleep(time.Second * 2)
+
 }
